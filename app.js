377e6a9faa2b40448f1969a6fa46ffe930442a51
Cassandra Torske <cassandra.torske@gmail.com>
2017-05-25 21:11:27 -0600

    stuff

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var index = require('./routes/index');
+var app = express();
+
+app.use('/', index);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!')
+})
+
+module.exports = app;
