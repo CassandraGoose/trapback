731ba6147a639c90fd0c3cd2ffb68caca13dd7e8
Cassandra Torske <cassandra.torske@gmail.com>
2017-05-25 21:58:32 -0600

    secret stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   console.log("HEY LISTEN")
-  sh play.sh
+  console.log("how do i make it run a script here bruh");
+  // sudo chmod +x hello-world.sh
+  // sh play.sh
 });
 
 module.exports = router;
